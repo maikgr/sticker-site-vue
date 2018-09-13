69c8fbc864ec4bb7938555ec3156ffc71dc7af5d
Maik G. Rantetasik <maik.gibran.rantetasik@kreasitindo.co.id>
2018-09-13 17:38:41 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <nav class="navbar has-background-dark" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <div class="navbar-item">
+          <span class="is-size-4 has-text-light">Stickers</span>
+        </div>
+      </div>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+@import url('https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.css');
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+html, body {
+  height: 100%;
+}
+
+.navbar-brand {
+  padding: 5px 0;
+}
+</style>
---
+++ src/components/Content.vue
@@ -0,0 +1,85 @@
+<template>
+  <section class="section has-background-light">
+    <div class="container">
+      <div class="columns">
+        <div class="column" v-for="v-bind:sticker in tests">
+          <div class="box has-text-centered">
+              <img class="thumbs" src="https://cdn.dribbble.com/users/851350/screenshots/5136810/family_time_4x.png" />
+            <div class="level caption">
+              <div class="level-left">
+                {{ sticker }}
+              </div>
+              <div class="level-right">
+                <font-awesome-icon icon="comment-dots" />
+                12
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Content',
+  data () {
+    return {
+      tests : ["aasd", "defeaf", "feqwdea"],
+      stickers : [{
+        "upload": {
+            "id": "140759547085193216",
+            "username": "VarZ",
+            "date": {
+                "$date": "2018-09-12T16:06:45.000Z"
+            }
+          },
+          "keyword": "kianajii",
+          "url": "https://cdn.discordapp.com/emojis/452458009222971412.png",
+      },
+      {
+        "upload": {
+          "id": "216604819173933056",
+          "username": "Grimmrist",
+          "date": {
+              "$date": "2018-09-13T02:33:58.000Z"
+          }
+        },
+        "keyword": "niiniicheer",
+        "url": "https://media.discordapp.net/attachments/474588210299666442/489624686276378645/unknown.png"
+      },
+      {
+         "upload": {
+          "id": "216604819173933056",
+          "username": "Grimmrist",
+          "date": {
+              "$date": "2018-09-13T02:51:28.000Z"
+          }
+        },
+        "keyword": "destroyschicksal",
+        "url": "https://media.discordapp.net/attachments/474588210299666442/489626636350783509/FB_IMG_15239440504806158.png"
+      }
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .section {
+    height: 90%;
+  }
+
+  .box img.thumbs {
+    width: 100%;
+    height: 100%;
+    min-width: 200px;
+    min-height: 150px;
+  }
+
+  .box div.caption {
+    margin: 0 0 -10px 0;
+  }
+</style>
