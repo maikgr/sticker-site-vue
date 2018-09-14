0a2adb80a7474bc6c3d1d775da7474f5adf3c965
MGibran <maikgibranr@gmail.com>
2018-09-14 10:46:45 +0700

    Fix loop key binding

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -2,12 +2,12 @@
   <section class="section has-background-light">
     <div class="container">
       <div class="columns">
-        <div class="column" v-for="v-bind:sticker in tests">
+        <div class="column" v-for="sticker in stickers" :key="sticker">
           <div class="box has-text-centered">
-              <img class="thumbs" src="https://cdn.dribbble.com/users/851350/screenshots/5136810/family_time_4x.png" />
+              <img class="thumbs" :src="sticker.url" />
             <div class="level caption">
               <div class="level-left">
-                {{ sticker }}
+                {{ sticker.keyword }}
               </div>
               <div class="level-right">
                 <font-awesome-icon icon="comment-dots" />
@@ -26,39 +26,38 @@ export default {
   name: 'Content',
   data () {
     return {
-      tests : ["aasd", "defeaf", "feqwdea"],
-      stickers : [{
-        "upload": {
-            "id": "140759547085193216",
-            "username": "VarZ",
-            "date": {
-                "$date": "2018-09-12T16:06:45.000Z"
-            }
-          },
-          "keyword": "kianajii",
-          "url": "https://cdn.discordapp.com/emojis/452458009222971412.png",
-      },
-      {
-        "upload": {
-          "id": "216604819173933056",
-          "username": "Grimmrist",
-          "date": {
-              "$date": "2018-09-13T02:33:58.000Z"
+      stickers: [{
+        'upload': {
+          'id': '140759547085193216',
+          'username': 'VarZ',
+          'date': {
+            '$date': '2018-09-12T16:06:45.000Z'
           }
         },
-        "keyword": "niiniicheer",
-        "url": "https://media.discordapp.net/attachments/474588210299666442/489624686276378645/unknown.png"
+        'keyword': 'kianajii',
+        'url': 'https://cdn.discordapp.com/emojis/452458009222971412.png'
       },
       {
-         "upload": {
-          "id": "216604819173933056",
-          "username": "Grimmrist",
-          "date": {
-              "$date": "2018-09-13T02:51:28.000Z"
+        'upload': {
+          'id': '216604819173933056',
+          'username': 'Grimmrist',
+          'date': {
+            '$date': '2018-09-13T02:33:58.000Z'
           }
         },
-        "keyword": "destroyschicksal",
-        "url": "https://media.discordapp.net/attachments/474588210299666442/489626636350783509/FB_IMG_15239440504806158.png"
+        'keyword': 'niiniicheer',
+        'url': 'https://media.discordapp.net/attachments/474588210299666442/489624686276378645/unknown.png'
+      },
+      {
+        'upload': {
+          'id': '216604819173933056',
+          'username': 'Grimmrist',
+          'date': {
+            '$date': '2018-09-13T02:51:28.000Z'
+          }
+        },
+        'keyword': 'destroyschicksal',
+        'url': 'https://media.discordapp.net/attachments/474588210299666442/489626636350783509/FB_IMG_15239440504806158.png'
       }
       ]
     }
@@ -73,10 +72,7 @@ export default {
   }
 
   .box img.thumbs {
-    width: 100%;
-    height: 100%;
-    min-width: 200px;
-    min-height: 150px;
+    height: 150px;
   }
 
   .box div.caption {
